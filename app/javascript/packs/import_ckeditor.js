6af6c570310d073e0404f07a73f2bf6d4e2f84e2
anthony <aahewins@protonmail.com>
2019-04-24 23:12:32 -0400

    More optimized article form with less wysiwig bloat

--- app/javascript/packs/import_ckeditor.js
+++ app/javascript/packs/import_ckeditor.js
@@ -1,17 +1,39 @@
 import CKEditorRenderer from "../src/CKEditorRenderer";
+import parseKatex from "../src/KatexParser";
 
 var CKEDITORS = {};
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {    
+    const getRenderTarget = function(node) {
+        var renderTarget = document.querySelector(node.dataset.target);
+
+        if (renderTarget == null && isUsingKatex)
+            throw new Error("Need data-target to perform rendering");
+
+        return renderTarget;
+    }
+
+    // Create a listener that renders HTML as its written in CKEditor
+    var renderer = new CKEditorRenderer()
     document.querySelectorAll('.wysiwig').forEach(node => {
         ClassicEditor.create(node)
             .then(instance => {
                 CKEDITORS[node.id] = instance;
                 return instance;
             })
-            .then(instance => new CKEditorRenderer().addEventListener(instance, node))
+            .then(instance => renderer.addEventListener(instance, node))
             .catch(error => console.error(error));
     });
+
+    // Create a listener that renders HTML as its written in some regular HTML node
+    document.querySelectorAll('.katex-input').forEach(node => {
+        if (!node.classList.contains("wysiwig")) {
+            const target = getRenderTarget(node);
+            node.addEventListener("input", function() {
+                target.innerHTML = parseKatex(node.value)
+            })
+        }
+    })
 });
 
 window.CKEDITORS = CKEDITORS;
