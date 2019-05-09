fc4996d38b8c7acac16c3a8ac428aa7bd618c86a
ahewins <ahewins@flexibleplan.com>
2019-05-09 11:45:57 -0400

    403 errors are getting caught now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
+require Rails.root.join 'lib/assets/permission'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+
+  rescue_from Permission::AccessDenied do
+    render file: "public/403", status: :forbidden
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
