7b28ef380dd09e48c1f1e7483ff4d9ce1d77766a
anthony <aahewins@protonmail.com>
2019-05-18 20:41:02 -0400

    Replace uid in favor of dom_id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def uid(model)
-    return nil if model.nil?
-    raise TypeError, "#{model} isn't a descendant of ActiveRecord::Base" unless model.class < ActiveRecord::Base
-    "#{model.class.to_s.downcase}#{model.id}"
-  end
 end
