fca19e513d1bfc0360eef47d931b1ad5849b9aa8
ahewins <ahewins@flexibleplan.com>
2019-06-13 17:33:01 -0400

    Made the seed file idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,31 @@
+#===============================================================================
+# This seed file should be idempotent.
+#===============================================================================
+
 ActiveRecord::Base.logger = Logger.new STDOUT
 
-User.create!(handle: "aahewins", password: "a!!!!!")
+unless User.exists?(handle: "aahewins")
+  User.create!(handle: "aahewins", password: "a!!!!!")
+end
 
-Tag.create!(name: "Mathematics",                  css: "blue calculator icon")
-Tag.create!(name: "discrete mathematics",         css: 'orange braille icon')
-Tag.create!(name: "group theory",                 css: 'red circle outline icon')
-Tag.create!(name: "linear algebra",               css: 'violet chess board icon')
+upsert = lambda do |name, css|
+  tag = Tag.find_or_create_by(name: name)
+  tag.update!(name: name, css: "blue calculator icon")
+end
 
-Tag.create!(name: "Computer Science",             css: "grey microchip icon")
-Tag.create!(name: "algorithms",                   css: 'green terminal icon')
-Tag.create!(name: "theoretical computer science", css: 'olive sitemap icon')
-Tag.create!(name: "cybersecurity",                css: 'blue shield alternate icon')
+upsert.call "mathematics",                  "blue calculator icon"
+upsert.call "discrete mathematics",         'orange braille icon'
+upsert.call "group theory",                 'red circle outline icon'
+upsert.call "linear algebra",               'violet chess board icon'
 
-Tag.create!(name: "Conspiracy",                   css: "red eye icon")
-Tag.create!(name: "flat earth",                   css: "black globe icon")
-Tag.create!(name: "9/11",                         css: "grey fighter jet icon")
-Tag.create!(name: "aliens",                       css: "yellow question circle outline icon")
+upsert.call "computer science",             "grey microchip icon"
+upsert.call "algorithms",                   'green terminal icon'
+upsert.call "theoretical computer science", 'olive sitemap icon'
+upsert.call "cybersecurity",                'blue shield alternate icon'
 
-Tag.create!(name: "Shitposts",                    css: "red thumbs down outline icon")
+upsert.call "conspiracy",                   "red eye icon"
+upsert.call "flat earth",                   "black globe icon"
+upsert.call "9/11",                         "grey fighter jet icon"
+upsert.call "aliens",                       "yellow question circle outline icon"
+
+upsert.call "shitposts",                    "red thumbs down outline icon"
