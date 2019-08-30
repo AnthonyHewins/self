18e8259c88c8ab573c6f5ac68f6211176aa0d4d3
anthony <aahewins@protonmail.com>
2019-08-29 21:53:55 -0400

    fixup mistakes with sitemap and integrate it automatically into capistrano

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,12 +1,9 @@
 SitemapGenerator::Sitemap.default_host = "http://sudo-bangbang.com"
 SitemapGenerator::Sitemap.public_path = 'public/sitemap'
 
-# The full path to your bucket
-SitemapGenerator::Sitemap.sitemaps_host = "https://#{'ENV["S3_BUCKET_NAME"]'}.s3.amazonaws.com"
-
 # The paths that need to be included into the sitemap.
 SitemapGenerator::Sitemap.create do
   Article.find_each do |article|
-    add article_path(article.slug_en, locale: :en, changefreq: :weekly, lastmod: article.updated_at)
+    add article_path(article.id, locale: :en, changefreq: :weekly, lastmod: article.updated_at)
   end
 end
