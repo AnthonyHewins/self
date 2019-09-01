75d754157b6fe6622496bfecbaa495cc4eb08a43
anthony <aahewins@protonmail.com>
2019-09-01 15:57:12 -0400

    harden rack attack more

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,14 +1,6 @@
 class Rack::Attack
   ### Throttle Spammy Clients ###
 
-  # If any single client IP is making tons of requests, then they're
-  # probably malicious or a poorly-configured scraper. Either way, they
-  # don't deserve to hog all of the app server's CPU. Cut them off!
-  #
-  # Note: If you're serving assets through rack, those requests may be
-  # counted by rack-attack and this throttle may be activated too
-  # quickly. If so, enable the condition to exclude them from tracking.
-
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
@@ -17,13 +9,6 @@ class Rack::Attack
   end
 
   ### Prevent Brute-Force Login Attacks ###
-
-  # The most common brute-force login attack is a brute-force password
-  # attack where an attacker simply tries a large number of emails and
-  # passwords to see if any credentials match.
-  #
-  # Another common method of attack is to use a swarm of computers with
-  # different IPs to try brute-forcing a password for a specific account.
 
   # Throttle POST requests to /login by IP address
   #
@@ -34,8 +19,8 @@ class Rack::Attack
     end
   end
 
-  blocklist('block anything php wise') do |req|
-    req.path.match(/.php\Z/i)
+  blocklist('block requests that are known to not be served via Rails') do |req|
+    req.path.match(/\.(php|cfg|cgi|asp)\Z/i)
   end
 
   # Throttle POST requests to /login by email param
